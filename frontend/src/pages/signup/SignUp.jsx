bbc5176454d2b0fd12e58af8a2319543f1e1dfb5
Ansh Modi <modi.102ansh@gmail.com>
2024-02-26 15:57:11 +0530

    2nd part completed-UI design

---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import GenderCheckbox from './GenderCheckBox';
+const SignUp = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
+        <h1 className="text-3x1 font-semibold text-center text-gray-300 ">
+          SignUp
+          <span className=' text-black '> Chat App</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input type="text" placeholder="John Doe" className="w-full input input-bordered h-10" />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input type="text" placeholder="JohnDoe" className="w-full input input-bordered h-10" />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10" />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input type="password" placeholder="Confirm Password" className="w-full input input-bordered h-10" />
+          </div>
+
+          <GenderCheckbox/>
+
+          <a href="" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+            Already have an account?
+          </a>
+          
+          <div>
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700 ">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp;
+
+
+//STARTER CODE
+// import React from 'react'
+// import GenderCheckbox from './GenderCheckBox';
+// const SignUp = () => {
+//   return (
+//     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
+//         <h1 className="text-3x1 font-semibold text-center text-gray-300 ">
+//           SignUp
+//           <span className=' text-black '> Chat App</span>
+//         </h1>
+
+//         <form>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Full Name</span>
+//             </label>
+//             <input type="text" placeholder="John Doe" className="w-full input input-bordered h-10" />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Username</span>
+//             </label>
+//             <input type="text" placeholder="JohnDoe" className="w-full input input-bordered h-10" />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Password</span>
+//             </label>
+//             <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10" />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Confirm Password</span>
+//             </label>
+//             <input type="password" placeholder="Confirm Password" className="w-full input input-bordered h-10" />
+//           </div>
+
+//           <GenderCheckbox/>
+
+//           <a href="" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+//             Already have an account?
+//           </a>
+          
+//           <div>
+//             <button className="btn btn-block btn-sm mt-2 border border-slate-700 ">Sign Up</button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default SignUp;
