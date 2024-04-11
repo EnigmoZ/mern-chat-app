5844783fbab5da1a30c8e0fd7efdc8785ad455ec
Ansh Modi <modi.102ansh@gmail.com>
2024-04-11 21:21:47 +0530

    protect Route error solved

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getUsersForSidebar } from '../controllers/user.controller.js'; 
-import protectRoute from '../middleware/protectroute.js';
+import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
